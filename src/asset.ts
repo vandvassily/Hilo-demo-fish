5b6dc95be3d2307e19eb5a0c90cc86f5b50c6360
vandvassily <zhs25427199@gmail.com>
2021-11-20 12:38:24 +0800

    feat: 完成预期功能

--- src/asset.ts
+++ src/asset.ts
@@ -1,35 +1,61 @@
 // @ts-nocheck
-import Hilo from "hilojs";
+import Hilo from 'hilojs';
+import fireImg from './images/fire.png';
+import numberImg from './images/number.png';
 
 const Asset = Hilo.Class.create({
   Mixes: Hilo.EventMixin,
   queue: null,
   bg: null,
   fish: null,
+  fireImg: null,
 
   load: function () {
     const resources = [
       {
-        id: "bg",
-        src: "https://hiloteam.github.io/Hilo/examples/images/map.jpg"
+        id: 'bg',
+        src: 'https://hiloteam.github.io/Hilo/examples/images/map.jpg'
       },
       {
-        id: "fish",
-        src: "https://hiloteam.github.io/Hilo/examples/images/fish.png"
+        id: 'fish',
+        src: 'https://hiloteam.github.io/Hilo/examples/images/fish.png'
+      },
+      {
+        id: 'fireImg',
+        src: fireImg
+      },
+      {
+        id: 'numberImg',
+        src: numberImg
       }
     ];
 
     this.queue = new Hilo.LoadQueue();
     this.queue.add(resources);
-    this.queue.on("complete", this.onComplete.bind(this));
+    this.queue.on('complete', this.onComplete.bind(this));
     this.queue.start();
   },
 
   onComplete: function (e) {
-    this.bg = this.queue.get("bg").content;
-    this.fish = this.queue.get("fish").content;
-    this.queue.off("complete");
-    this.fire("complete");
+    this.bg = this.queue.get('bg').content;
+    this.fish = this.queue.get('fish').content;
+    this.fireImg = this.queue.get('fireImg').content;
+    this.numberImg = this.queue.get('numberImg').content;
+
+    this.numberGlyphs = {
+      0: { image: numberImg, rect: [0, 0, 60, 91] },
+      1: { image: numberImg, rect: [61, 0, 60, 91] },
+      2: { image: numberImg, rect: [121, 0, 60, 91] },
+      3: { image: numberImg, rect: [191, 0, 60, 91] },
+      4: { image: numberImg, rect: [261, 0, 60, 91] },
+      5: { image: numberImg, rect: [331, 0, 60, 91] },
+      6: { image: numberImg, rect: [401, 0, 60, 91] },
+      7: { image: numberImg, rect: [471, 0, 60, 91] },
+      8: { image: numberImg, rect: [541, 0, 60, 91] },
+      9: { image: numberImg, rect: [611, 0, 60, 91] }
+    };
+    this.queue.off('complete');
+    this.fire('complete');
   }
 });
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import Hilo from "hilojs";
-import _ from "lodash";
-import Asset from "./asset";
-import { getRotateAngle } from "./utils";
+import Hilo from 'hilojs';
+import _ from 'lodash';
+import Asset from './asset';
+import { getRotateAngle } from './utils';
 
 // 移除相同目标ID的动画
 const removeTweenMove = (id: string) => {
@@ -17,11 +17,11 @@ const removeTweenMove = (id: string) => {
 
 // 等级映射
 const levelMap = {
-  "1": 10,
-  "2": 20,
-  "3": 30,
-  "4": 40,
-  "5": 50
+  '1': 5,
+  '2': 10,
+  '3': 15,
+  '4': 20,
+  '5': 25
 };
 
 interface GameType {
@@ -36,13 +36,20 @@ interface GameType {
   ticker: Hilo.Ticker | null;
   bg?: Hilo.View;
   fish?: Hilo.View;
+  particle?: Hilo.ParticleSystem;
+  levelPanel?: Hilo.BitmapText;
 
   initStage: () => void;
   initBackground: () => void;
   initFish: () => void;
+  initParticle: () => void;
+  initLevelPanel: () => void;
   onUserInput: (e: MouseEvent) => void;
   onFishMoveCompletion: () => void;
   init: () => void;
+
+  levelUp: () => void;
+  stageOnUpdate: () => void;
 }
 
 const game: GameType = {
@@ -54,13 +61,14 @@ const game: GameType = {
 
   stage: null,
   fish: undefined,
+  particle: undefined,
   ticker: null,
 
   init: function () {
     // @ts-ignore
     this.asset = new Asset();
-    this.asset.on("complete", () => {
-      this.asset.off("complete");
+    this.asset.on('complete', () => {
+      this.asset.off('complete');
       this.initStage();
     });
 
@@ -71,11 +79,11 @@ const game: GameType = {
     // 创建舞台
     this.stage = new Hilo.Stage({
       // @ts-ignore
-      id: "mainStage",
-      renderType: "canvas",
+      id: 'mainStage',
+      renderType: 'canvas',
       width: this.width,
       height: this.height,
-      container: document.getElementById("stageContainer") as HTMLElement
+      container: document.getElementById('stageContainer') as HTMLElement
     });
 
     // 启动定时器，60帧
@@ -90,23 +98,25 @@ const game: GameType = {
 
     // 监听点击事件
     if (document.addEventListener) {
-      document.addEventListener("click", (e: MouseEvent) => {
+      document.addEventListener('click', (e: MouseEvent) => {
         this.onUserInput(e);
       });
     }
 
     // 绑定舞台更新
-    this.stage.onUpdate = function () {};
+    this.stage.onUpdate = this.stageOnUpdate.bind(this);
 
     // 初始化其他组件
     this.initBackground();
     this.initFish();
+    this.initParticle();
+    this.initLevelPanel();
   },
 
   initBackground: function () {
     this.bg = new Hilo.Bitmap({
       // @ts-ignore
-      id: "bg",
+      id: 'bg',
       width: 1280,
       height: 720,
       image: this.asset.bg
@@ -133,7 +143,7 @@ const game: GameType = {
 
     // 创建精灵图容器
     this.fish = new Hilo.Container({
-      id: "fish",
+      id: 'fish',
       x: 80,
       y: 50,
       width: 174,
@@ -142,7 +152,7 @@ const game: GameType = {
       pivotY: 48,
       children: [
         new Hilo.Sprite({
-          frames: atlas.getSprite("fish"),
+          frames: atlas.getSprite('fish'),
           x: 0,
           y: 0,
           alpha: 0.8,
@@ -155,6 +165,91 @@ const game: GameType = {
     });
 
     this.stage?.addChild(this.fish);
+  },
+
+  // 创建粒子效果
+  initParticle() {
+    const { fireImg } = this.asset;
+    this.particle = new Hilo.ParticleSystem({
+      x: 0,
+      y: 0,
+      emitNum: 20,
+      emitTime: 1,
+      particle: {
+        frame: [
+          [75, 236, 7, 11],
+          [119, 223, 7, 17],
+          [90, 223, 22, 17],
+          [51, 202, 17, 46],
+          [94, 59, 34, 59],
+          [60, 160, 34, 42],
+          [30, 99, 30, 99],
+          [7, 240, 7, 11],
+          [119, 206, 7, 17],
+          [90, 206, 22, 17],
+          [111, 160, 17, 46],
+          [60, 59, 34, 59],
+          [94, 118, 34, 42],
+          [30, 0, 30, 99],
+          [68, 236, 7, 11],
+          [112, 223, 7, 17],
+          [68, 219, 22, 17],
+          [94, 160, 17, 46],
+          [94, 0, 34, 59],
+          [60, 118, 34, 42],
+          [0, 99, 30, 99],
+          [0, 240, 7, 11],
+          [112, 206, 7, 17],
+          [68, 202, 22, 17],
+          [34, 198, 17, 46],
+          [60, 0, 34, 59],
+          [0, 198, 34, 42],
+          [0, 0, 30, 99]
+        ],
+        image: fireImg,
+        life: 22,
+        alphaV: -0.01,
+        vxVar: 300,
+        vyVar: 300,
+        axVar: 200,
+        ayVar: 200,
+        scale: 0.5,
+        rotationVar: 360,
+        rotationVVar: 4,
+        pivotX: 0.5,
+        pivotY: 0.5
+      },
+      emitterX: 300,
+      emitterY: 300
+    });
+    this.stage?.addChild(this.particle);
+  },
+
+  // 初始化等级面板
+  initLevelPanel: function () {
+    this.levelPanel = new Hilo.BitmapText({
+      id: 'score',
+      glyphs: this.asset.numberGlyphs,
+      textAlign: 'center',
+      x: 400,
+      y: 0,
+      text: this.level
+    });
+
+    this.stage?.addChild(this.levelPanel);
+  },
+
+  levelUp: function () {
+    if (this.level < 6) {
+      this.level++;
+      this.speed += 100;
+      this.count = 0;
+      this.particle?.start();
+      this.levelPanel?.setText(String(this.level));
+      setTimeout(() => {
+        this.particle?.stop();
+      }, 5000);
+    }
   },
 
   onUserInput(e) {
@@ -178,7 +273,8 @@ const game: GameType = {
       const distance = Math.sqrt(absX * absX + absY * absY);
       const duration = (distance * 1000) / speed;
 
-      removeTweenMove("fish");
+      // TODO: 可以优化，不用每次都创建一个新的Tween；例如，可以使用一个全局的Tween，然后每次都更新目标值
+      removeTweenMove('fish');
       Hilo.Tween.to(
         this.fish,
         {
@@ -195,24 +291,18 @@ const game: GameType = {
     }
   },
 
+  stageOnUpdate() {},
+
   onFishMoveCompletion() {
-    console.log("移动成功");
+    console.log('移动成功');
     console.log(this.level);
     if (this.level > 5) return;
     // @ts-ignore
-    if (this.count <= levelMap[this.level + ""]) {
+    if (this.count < levelMap[this.level + '']) {
       this.count++;
-
-      // @ts-ignore
-      if (this.count > levelMap[this.level + ""]) {
-        this.count = 1;
-        this.level++;
-      }
+    } else {
+      this.levelUp();
     }
-
-    console.log(this.level, this.count);
-    // TODO: 等级处理
-    // const keys = _.keysIn(Hilo.Tween);
   }
 };
 
